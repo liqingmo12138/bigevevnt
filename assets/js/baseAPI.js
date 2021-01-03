d5b127d68cfb329fd40801e8b19b10d8da8343d5
liqingmo <1973455263@qq.com>
2021-01-03 18:57:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,19 @@ $.ajaxPrefilter(function (options) {
 options.url="http://ajax.frontend.itheima.net"+options.url
 
 
+    if (options.url.indexOf('/my/')!==-1) {
+        options.headers = {
+            Authorization:localStorage.getItem('token')||''
+        }
+    }
+
+    //全局同一挂在complete 回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //   1.强制清 空 token
+            localStorage.removeItem('token')
+// 2.强制跳转到登陆页面
+            location.href ='/login.html'
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 $(function () {
     
+
+
     // 实现退出功能
     var layer = laui.layer;
     //点击按钮，实现退出
@@ -20,5 +22,49 @@ $(function () {
 
 
 
-})
+
 // 获取用户的基本信息
+    getUserInfo();
+
+    function getUserInfo () {
+        $.ajax({
+            method: "GET",
+         url:'/my/userinfo',
+            headers: {
+ Authorization: localStorage.getItem('token') || ''
+            },
+            success: function (res) {
+                if (res.status !==0) {
+                    return layui.layer.msg('获取用户信息失败！')
+                }
+                //调用renderAvatar 渲染用户的头像
+                renderAvatar(res.data)
+            }
+
+        })
+    }
+    //定义render Avatar 函数  
+    // 渲染用户的头像
+    function renderAvatar(user) {
+        // 1.获取用户的名称
+        var name = user.nickname || user.username
+        // 2.设置欢迎的文本
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+        // 3.按需渲染用户的头像
+        if (user.user_pic !==null) {
+        //  3.1渲染图片头像
+            $('.layui-nav-img')
+                .attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+            
+        } else {
+            // 3.2渲染文本头像
+            $('.layui-nav-img').hide()
+            var lirst = name[0].toUpperCase()
+            $('.text-avata').html(first).show()
+        }
+    }
+
+    
+    
+})
