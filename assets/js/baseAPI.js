85ffa3612de1e54e0103c6caf2815b08e52ddc69
liqingmo <1973455263@qq.com>
2021-01-04 20:56:03 +0800

    个人中心的开发与之前的修改

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,21 +1,22 @@
-$.ajaxPrefilter(function (options) {
-    
-options.url="http://ajax.frontend.itheima.net"+options.url
 
-
-    if (options.url.indexOf('/my/')!==-1) {
-        options.headers = {
-            Authorization:localStorage.getItem('token')||''
-        }
+$.ajaxPrefilter(function(options) {
+ 
+  // options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+  
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
     }
+  }
 
-    //全局同一挂在complete 回调函数
-    options.complete = function (res) {
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-        //   1.强制清 空 token
-            localStorage.removeItem('token')
-// 2.强制跳转到登陆页面
-            location.href ='/login.html'
-        }
+  options.complete = function(res) {
+
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+
+      localStorage.removeItem('token')
+
+      location.href = '/login.html'
     }
+  }
 })
